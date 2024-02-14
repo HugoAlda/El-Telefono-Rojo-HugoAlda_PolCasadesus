1f505b85c6d6f5e77ed3b477c88dcb50f17d688b
HugoAlda <hugoa1204@gmail.com>
2024-02-14 13:17:59 +0100

    v inicio php

--- index.php
+++ index.php
@@ -17,6 +17,18 @@
         
         <h1>Telefono Rojo</h1>
 
+        <div class="contenedor-telefono">
+            <img class="telefono" src="./img/telefono-rojo.webp">
+        </div>
+
+        <p>¡Has recibido una llamada del telefono rojo! ¿La vas a cojer?</p>        
+
+        <?php
+
+        
+
+        ?>
+
     </body>
 
 </html>
