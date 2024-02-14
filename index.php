4a741f4c2bc6350fd7b33d6eed0d8672ec2cdcd5
HugoAlda <hugoa1204@gmail.com>
2024-02-14 13:38:33 +0100

    v1 php

--- index.php
+++ index.php
@@ -8,6 +8,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         
         <link rel="stylesheet" href="./css/style.css">
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet">
         
         <title>Telefono Rojo</title>
 
@@ -21,11 +25,27 @@
             <img class="telefono" src="./img/telefono-rojo.webp">
         </div>
 
-        <p>¡Has recibido una llamada del telefono rojo! ¿La vas a cojer?</p>        
+        <div class="contenedor-form">
+            <p>¡Has recibido una llamada del telefono rojo! ¿La vas a cojer?</p>        
+
+            <form action="./index.php" method="GET">
+
+                <button class="btn">
+                    <label>Si</label>
+                </button>
+
+                <button class="btn">
+                    <label>No</label>
+                </button>
+
+            </form>
+        </div>
+
+        
 
         <?php
 
-        
+
 
         ?>
 
