bb84c482ee05e845b5595a09a7155bc14abfaa8b
HugoAlda <hugoa1204@gmail.com>
2024-02-15 13:42:50 +0100

    v1 rendicion

--- decision/si/reglas-juego.php
+++ decision/si/reglas-juego.php
@@ -5,6 +5,12 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet">
+
+        <link rel="stylesheet" href="./../../css/style-si.css">
+        
         <title>Reglas del Juego</title>
 
     </head>
