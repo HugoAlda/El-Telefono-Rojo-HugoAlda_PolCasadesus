007adf3be7d8d5a2e9a98bb578523a7a955f5c3f
HugoAlda <hugoa1204@gmail.com>
2024-02-20 12:03:09 +0100

    v2 loding.php

---
+++ decision/si/preguntas/loading.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="./../../../css/style-loading.css">
+
+    <title>Pantalla de carga</title>
+
+</head>
+
+<body>
+    
+    <h3>Cargando...</h3>
+    
+    <div class="barra">
+        
+        <div class="progreso" id="progreso"></div>
+
+    </div>
+
+    <script src="script.js"></script>
+
+</body>
+
+</html>
