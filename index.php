5ceb2a16d3e75e3be494a0ac47f372f95554fb2a
HugoAlda <hugoa1204@gmail.com>
2024-03-01 09:54:54 +0100

    index.php y styles.css

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
-
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
     <link rel="stylesheet" href="./css/style.css">
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Protest+Revolution&display=swap" rel="stylesheet">
-    
     <title>Teléfono Rojo</title>
 </head>
-<br>
 <body>
     <div class="contenedor-pantalla">
         <h1>Teléfono Rojo</h1>
         <div class="contenedor-telefono">       
-            <img class="telefono" src="./img/telefono-rojo.png">
+            <img class="telefono" src="./img/telefono-rojo.png" alt="Teléfono Rojo">
         </div>
-        <br>
         <div class="contenedor-form">
             <p>¡Has recibido una llamada del teléfono rojo! ¿La vas a coger?</p>        
             <form action="./recibir/recibir.php" method="GET">
@@ -32,3 +28,4 @@
         </div>
     </div>
 </body>
+</html>
