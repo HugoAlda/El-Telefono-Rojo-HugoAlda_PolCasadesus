bceba1d4da45486097f43a2e9770ee8433211619
HugoAlda <Hugoa1204@gmail.com>
2024-03-12 11:31:18 +0100

    organización y reestructuracion carpeta pruebas

---
+++ decision/si/pruebas/pruebas/prueba2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Pregunta 1</title>
+
+</head>
+<body>
+
+    <h1>Pregunta 1</h1>
+
+    <form action="verificar_respuesta.php" method="POST">
+
+        <input type="hidden" name="pregunta" value="1">
+
+        <input type="text" name="respuesta">
+
+        <button type="submit">Verificar</button>
+
+    </form>
+    
+</body>
+</html>
